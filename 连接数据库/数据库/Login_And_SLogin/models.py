135ca25f5b58c59236496afba8e72bf0629b8948
kongbai926 <114583484+kongbai926@users.noreply.github.com>
2023-11-15 19:38:21 +0800

    Delete 连接数据库 directory

--- 连接数据库/数据库/Login_And_SLogin/models.py
+++
@@ -1,9 +0,0 @@
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class UserTable(AbstractUser):
-
-    age = models.IntegerField(blank=True)  # 用户年龄 blank设置为True，可以不填，默认使用none填充数据库
-    sex = models.CharField(max_length=2, blank=True)  # 用户性别
