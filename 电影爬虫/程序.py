2471ae502a46811cfaa687bd6da8d206f3981d6a
kongbai926 <114583484+kongbai926@users.noreply.github.com>
2023-11-15 19:33:49 +0800

    Delete 电影爬虫 directory

--- 电影爬虫/程序.py
+++
@@ -1,9 +0,0 @@
-#-*- coding:UTF-8 -*-
-url = "https://okjx.cc/?url="
-import sys
-import webbrowser
-sys.path.append("libs")
-urls = input("请输入电影链接")#输入电影链接在线观看影片
-print(url+urls)
-webbrowser.open(url+urls)
-print(webbrowser.get())
